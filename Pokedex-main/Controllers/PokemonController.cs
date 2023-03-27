06e274bc6d610b7092464294ba897fbeb39672ec
Samuel Esteves <saamuelsiilva215@gmail.com>
2023-03-26 21:36:15 -0300

    1

---
+++ Pokedex-main/Controllers/PokemonController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using System.Text.Json;
+
+namespace Pokedex.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public async Task<IActionResult> Index(int? pagina = 0)
+        {
+            const string apiUrl = "http://pokeapi.co/api/v2/pokemon";
+            int limit = 200;
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"{apiUrl}?limit={limit}&offset={limit * pagina}");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<PokemonApiResponse>(content);
+
+            int? i = limit * pagina;
+
+            var pokemons = new List<Pokemon>();
+            foreach(var p in result.Results)
+            {
+                i++; 
+
+                Pokemon pokemon = new();
+                pokemon.Name = p.Name;
+                pokemon.Url = $"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{((i < 1000) ? i?.ToString("#000") : i)}.png";
+
+                pokemons.Add(pokemon);
+            }
+
+            return View(pokemons);
+        }
+
+    }
+}
---
+++ Pokedex-main/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Pokedex.Models
+{
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
