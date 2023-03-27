b4af85e1ab51e3a53a56a57034f237926aff3251
Samuel Esteves <100425955+samuelesteves@users.noreply.github.com>
2023-03-27 19:36:07 -0300

    Modal

---
+++ pokedex/Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace pokedex.Models
+{
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ pokedex/Models/PokemonApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace pokedex.Models
+{
+    public class PokemonApiResponse
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Pokemon> Results { get; set; }
+    }
+}
